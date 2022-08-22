5075dbea644159cd6d7a66c29f97f18f49e2a666
Taro Serigano <taroserigano@gmail.com>
2022-08-22 13:38:27 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,14 +22,15 @@ io.on('connect', (socket) => {  // when connected to the URL,
 
     socket.join(user.room);  // join the user to the room  
 
-    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`}); 
-    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});   // to "person who joined", system says "Welcome to room"...
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` }); // to everybody else, notifies someone joined. someone = admin 
 
-    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) }); // then send ginal about users in the room 
 
     callback();
   });
-
+  
+  // send msg in the user's room 
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id);
 
